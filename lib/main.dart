86361249e2ee277491d1b8d01ea87a1ee22f06dd
Sirintip Klaiklueng <koomkun_s@silpakorn.edu>
2022-04-08 17:15:05 +0700

    edit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
 // import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 // import 'package:http/http.dart' as http;
@@ -6,13 +5,15 @@ import 'package:flutter/material.dart';
 import "first.dart";
 
 
-void main() => runApp(MaterialApp(
+void main() => runApp(const MaterialApp(
   debugShowCheckedModeBanner: false,
   title: "Weather App",
   home: Home(),
 ));
 
 class Home extends StatefulWidget {
+  const Home({Key? key}) : super(key: key);
+
   @override
   _HomeState createState() => _HomeState();
 }
